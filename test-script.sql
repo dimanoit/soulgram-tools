0443c7cafacde986214042ff195a5fd9aacf387f
Дима Киреев <dimanoit@gmail.com>
2022-04-20 17:42:49 +0300

    add test script

---
+++ test-script.sql
@@ -0,0 +1,13 @@
+USE master
+DROP DATABASE [Soulgram.AccountManage]
+DROP DATABASE [Soulgram.Identity]
+
+
+USE [Soulgram.AccountManage] 
+
+SELECT * FROM dbo.UserInfos
+
+
+SELECT * FROM dbo.Hobbies
+
+SELECT * FROM dbo.UserHobbies
