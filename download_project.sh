ffe04a759055e76c21bf4f9df92d3966e2eb465f
dimanoit <dimanoit@gmail.com>
2022-12-14 22:31:32 +0100

    update: postman collection and download project script

--- download_project.sh
+++ download_project.sh
@@ -11,6 +11,7 @@ git clone https://github.com/dimanoit/soulgram.eventbus.git &
 git clone https://github.com/dimanoit/soulgram.eventbus.rabbitMq.git &
 git clone https://github.com/dimanoit/soulgram-file-manager.git &
 git clone https://github.com/dimanoit/soulgram-logging.git &
-git clone https://github.com/dimanoit/soulgram-identity.git 
+git clone https://github.com/dimanoit/soulgram-identity.git &
+git clone https://github.com/dimanoit/soulgram-chat.git
 
 echo 'Now you can close the bash'
